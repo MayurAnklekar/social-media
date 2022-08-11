2074ea716a9b207cfa627733b21765651f3376e4
MayurAnklekar <mayuranklekar1010@gmail.com>
2022-08-11 14:28:11 +0530

    Tailwind setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from "./App";
+import './index.css'
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
-}
-
-export default index
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Router>
+  <App />
+</Router>
+);
