f346f40a8ac2ccdc60424d5f3630fbdf818541b0
Chandan Kasamsetty <34879929+chandan2300@users.noreply.github.com>
2022-08-13 12:58:33 +0530

    basic post operations added

---
+++ backend/controllers/post.js
@@ -0,0 +1,63 @@
+const { BadRequestError, NotFoundError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
+const Post = require("../models/Post");
+const User = require("../models/User");
+const cloudinary = require("cloudinary").v2;
+const uploadImage = require("../utils/uploadImage");
+
+const options = { new: true, runValidators: true };
+
+const createPost = async (req, res) => {
+  const { caption } = req.body;
+  const { id } = req.user;
+  let image = req.files?.image || "";
+  if (!caption && !image)
+    throw new BadRequestError("Expected a caption or image");
+  if (image) {
+    const { secure_url: src, public_id } = await uploadImage(image);
+    image = { src, publicID: public_id };
+  }
+  const user = await User.findById(id);
+  //   console.log(req.body);
+  const post = await Post.create({
+    caption,
+    image,
+    createdBy: id,
+    userDetails: { name: user.name, image: user.profileImage },
+  });
+  res.status(StatusCodes.CREATED).json({ post });
+};
+
+const getPosts = async (req, res) => {
+  const { id, query = "", page = "1", userId = "" } = req.query;
+  if (id) {
+    const post = await Post.findById(id);
+    if (!post) throw new NotFoundError(`No post with id${id}`);
+    res.status(StatusCodes.OK).json({ post });
+  } else {
+    const limitCount = query ? Infinity : 10;
+    const skipCount = limitCount === 10 ? (+page - 1) * limitCount : Infinity;
+    const _query = {};
+    if (query) _query.caption = new RegExp(query, "i");
+    if (userId) _query.createdBy = userId;
+    const posts = await Post.find(_query)
+      .sort("-createdAt")
+      .limit(limitCount)
+      .skip(skipCount);
+    res.status(StatusCodes.OK).json({ posts, page: +page });
+  }
+};
+
+const deletePost = async (req, res) => {
+  const { id } = req.params;
+  const post = await Post.findOneAndDelete({ _id: id, createdBy: req.user.id });
+  post.image.publicID &&
+    (await cloudinary.uploader.destroy(post.image.publicID));
+  res.status(StatusCodes.OK).json({ post });
+};
+
+module.exports = {
+  createPost,
+  getPosts,
+  deletePost,
+};
