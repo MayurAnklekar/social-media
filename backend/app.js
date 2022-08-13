ef381527bfca83e9d224c078f6f4d49b11dc6987
Chandan Kasamsetty <34879929+chandan2300@users.noreply.github.com>
2022-08-13 13:01:04 +0530

    app.js updated

--- backend/app.js
+++ backend/app.js
@@ -37,12 +37,16 @@ cloudinary.config({
 //routers
 
 const authRouter = require("./routes/auth");
+const postRouter = require("./routes/post");
 
 //middleware
 
 const authorizationMiddleware = require("./middleware/authorization");
 
+// Routes
+
 app.use("/api/v1/auth", authRouter);
+app.use("/api/vi/posts", authorizationMiddleware, postRouter);
 
 const start = async () => {
   try {
