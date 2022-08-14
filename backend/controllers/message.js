1c05d293945e12e71064310f74ef50d9f3b94143
Chandan Kasamsetty <34879929+chandan2300@users.noreply.github.com>
2022-08-14 18:05:01 +0530

    message controller added

---
+++ backend/controllers/message.js
@@ -0,0 +1,20 @@
+const { StatusCodes } = require("http-status-codes");
+const Message = require("../models/Message");
+const Chat = require("../models/Chat");
+
+const getMessages = async (req, res) => {
+  const { chatId } = req.query;
+  console.log(req.query);
+  const messages = await Message.find({ chatID: chatId });
+  res.status(StatusCodes.OK).json({ messages });
+};
+
+const deleteM = async (req, res) => {
+  if (req.user.id === "61aed3a59b979260987c4fbf") {
+    const messages = await Chat.deleteMany({ lastMessage: { $exists: false } });
+    res.status(StatusCodes.OK).json({ messages });
+  }
+  res.json({ msg: "admin delete" });
+};
+
+module.exports = { getMessages, deleteM };
