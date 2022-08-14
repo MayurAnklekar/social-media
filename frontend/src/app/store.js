57544937e7ff00d3b11dfc8c46b0903f3de7b0aa
MayurAnklekar <mayuranklekar1010@gmail.com>
2022-08-14 10:48:46 +0530

    Added post services and updated postSlice

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -3,6 +3,7 @@ import userReducer from "../features/userSlice";
 import modalReducer from '../features/modalSlice';
 import usersReducer from '../features/usersSlice';
 import messageReducer from '../features/messageSlice';
+import postReducer from '../features/postSlice';
 
 export const store = configureStore({
     reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
         modal: modalReducer,
         users: usersReducer,
         message: messageReducer,
+        post: postReducer,
     }
 });
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,30 +1,38 @@
-import React from 'react'
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import InfinityScroll from "../../components/InfinityScroll/InfinityScroll";
 import ProfileCard from "../../components/ProfileCard/ProfileCard";
+import { fetchPostsService } from "../../services/postServices";
+import { setAllPosts } from "../../features/postSlice";
 import "./home.css";
 
 const Home = () => {
   const dispatch = useDispatch();
   const {
-		user: { id },
-	} = useSelector(state => state);
-
+    post: {
+      allPosts: { posts, page },
+    },
+    user: { id },
+  } = useSelector((state) => state);
 
   const getNextPage = async () => {
-	};
+    const data = await fetchPostsService({ page: page + 1 });
+    dispatch(setAllPosts({ posts: posts.concat(data.posts), page: data.page }));
+    return data.posts.length;
+  };
 
   return (
     <section className="home">
       <div className="home__left">
-				<ProfileCard id={id} isOwnProfile />
-			</div>
+        <ProfileCard id={id} isOwnProfile />
+      </div>
       <InfinityScroll getNextPage={getNextPage}>
-      <main className="home__center">
-      </main>
+        <main className="home__center">
+          Hello  
+        </main>
       </InfinityScroll>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
