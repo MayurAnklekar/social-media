072f33f677e619553cf34a3b23ffff5c50ced499
Chandan Kasamsetty <34879929+chandan2300@users.noreply.github.com>
2022-08-17 16:46:07 +0530

    email check complete

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -10,7 +10,7 @@ const validator = require("email-validator");
 const register = async (req, res) => {
   console.log(req.body);
   let user = await User.findOne({ email: req.body.email });
-  if (user) throw new BadRequestError("User already exists");
+  if (user) res.status(StatusCodes.BAD_REQUEST).json("User already exists");
   if (!validator.validate(req.body.email))
     res.status(StatusCodes.BAD_REQUEST).json("Invalid Email");
   user = await User.create({ ...req.body });
@@ -28,12 +28,14 @@ const login = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password)
     throw new BadRequestError("Please provide email and password");
+  console.log("password", req.body.password);
+  console.log("email", req.body.email);
   const user = await User.findOne({ email: req.body.email });
-  if (!user) throw new NotFoundError("User doesn't exist");
+  if (!user) res.status(StatusCodes.BAD_REQUEST).json("User does not exist");
   const isPasswordCorrect = await user.comparePassword(req.body.password);
   if (!isPasswordCorrect)
     res
-      .status(StatusCodes.BAD_REQUEST)
+      .status(StatusCodes.UNAUTHORIZED)
       .json("It's Ezio's password!! Enter yours");
 
   const { _id: id, name, profileImage } = user;
